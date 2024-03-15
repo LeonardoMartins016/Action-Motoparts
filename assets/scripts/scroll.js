f11ab83bb398d1b8c50547be344317edac3731db
Luigi Verdério <luigiverderio@hotmail.com>
2024-03-15 08:28:22 -0300

    Adicionando os efeitos de ir aparecendo os elementos

---
+++ assets/scripts/scroll.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.scroll', {  
+origin: 'bottom', // Animar de baixo para cima
+distance: '20px', // Distância de deslocamento
+duration: 1000, // Duração da animação
+delay: 0, // Atraso antes da animação começar
+easing: 'ease-in-out', // Tipo de transição
+interval: 0 // Intervalo entre múltiplos elementos});
+});
