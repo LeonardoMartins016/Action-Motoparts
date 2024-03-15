90bef51bd41a3fd78eef5a6516660913793b9e4c
Luigi Verdério <luigiverderio@hotmail.com>
2024-03-15 04:52:42 -0300

    Estilizando tudo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,8 @@
 var navLinks = document.querySelector('.nav-links');
 var menu = document.querySelector(".menu");
+var body = document.querySelector('body');
 
 menu.addEventListener('click', () =>{
-  navLinks.classList.toggle('ativo')
+  navLinks.classList.toggle('ativo');
+  body.classList.toggle('lock');
 })
