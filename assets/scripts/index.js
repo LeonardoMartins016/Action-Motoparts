89288a962c8caae5d57285c6569f3db04a15982a
LeonardoMartins016 <ds.45908023841@gmail.com>
2024-03-15 12:35:20 -0300

    deixando a pagina de produtos responsiva

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,15 @@
 var navLinks = document.querySelector('.nav-links');
 var menu = document.querySelector(".menu");
 var body = document.querySelector('body');
+var descricao = document.querySelector('.descricao-product');
+var produtos = document.querySelector('.products')
 
 menu.addEventListener('click', () =>{
   navLinks.classList.toggle('ativo');
   body.classList.toggle('lock');
   menu.classList.toggle('ativo');
+})
+
+produtos.addEventListener('click', () => {
+  descricao.classList.toggle('aparece')
 })
